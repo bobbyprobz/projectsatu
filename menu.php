78061eb4fb12c6578454af7c7eee51babd96b129
bobbyprobz <subscribe@probo.me>
2015-11-05 13:06:07 +0700

    tambahan di nama

---
+++ menu.php
@@ -0,0 +1,88 @@
+        <div class="topbar"><a href="index.php"><img src="images/logoblekros.png"></a></div>
+        <div class="pusher"></div>
+        <div id="sidebar-wrapper">
+            <div class="btnmenu" id="btnmenu"><span class="glyphicon glyphicon-menu-hamburger"></span></div>
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+              <div class="panel panel-default">
+                <div class="panel-heading panel-order" role="tab" id="headingZero">
+                  <h4 class="panel-title">
+                    <a class="ordernow" href="index.php">ORDER NOW</a>
+                  </h4>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingOne">
+                  <h4 class="panel-title">
+                    <a href="index.php">home</a>
+                  </h4>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingOne">
+                  <h4 class="panel-title">
+                    <a href="whoami.php">who am i</a>
+                  </h4>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                  <h4 class="panel-title">
+                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      package <span class="caret"></span>
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                  <div class="panel-body">
+                     <ul>
+                        <li><a href="packagediet.php">blekros diet program</a></li>
+                        <li><a href="packagemaintenance.php">blekros maintenance program</a></li>
+                        <li><a href="packagenipka.php">blekros x nipka</a></li>
+                     </ul>                    
+                  </div>
+                </div>
+              </div>
+              
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                  <h4 class="panel-title">
+                    <a href="deliveryterms.php">delivery terms</a>
+                  </h4>
+                </div>
+              </div>
+
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                  <h4 class="panel-title">
+                    <a href="contact.php">contact</a>
+                  </h4>
+                </div>
+              </div>
+
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                  <h4 class="panel-title">
+                    <a href="blog.php">blog</a>
+                  </h4>
+                </div>
+              </div>
+
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                  <h4 class="panel-title">
+                    <a href="login.php">login</a>
+                  </h4>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                  <h4 class="panel-title">
+                    <a href="register.php">register</a>
+                  </h4>
+                </div>
+              </div>
+
+
+            </div>            
+        </div>
